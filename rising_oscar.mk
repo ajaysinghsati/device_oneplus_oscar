59e067e8e81c10d668e01284ba655c74234f3ae3
ajaysinghsati <125855251+ajaysinghsati@users.noreply.github.com>
2024-01-03 05:58:34 +0530

    Changes for RisingOS
    Signed-off-by: Alvin Francis <ajaysinghsati@gmail.com>

--- rising_oscar.mk
+++ rising_oscar.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oscar device
 $(call inherit-product, device/oneplus/oscar/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common risingOS.
+$(call inherit-product, vendor/rising/config/rising.mk)
 
-PRODUCT_NAME := aosp_oscar
+PRODUCT_NAME := rising_oscar
 PRODUCT_DEVICE := oscar
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
